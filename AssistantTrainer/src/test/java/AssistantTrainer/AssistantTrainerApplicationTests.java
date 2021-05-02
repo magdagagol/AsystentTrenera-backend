6ddc495372c48c62c284eb8c045289313576cf27
magdalena.gagol <magdalena.gagol@zeto.lublin.pl>
2021-05-02 22:54:02 +0200

    struktura bazowa

---
+++ AssistantTrainer/src/test/java/AssistantTrainer/AssistantTrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package AssistantTrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistantTrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
